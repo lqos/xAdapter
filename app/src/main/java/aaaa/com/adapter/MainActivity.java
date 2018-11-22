e88db51dc42597fff95fa73a58e289ccbdce2347
Liuliqiang <mylike_45@163.com>
2018-11-22 17:12:26 +0800

    我的提交2

--- app/src/main/java/aaaa/com/adapter/MainActivity.java
+++ app/src/main/java/aaaa/com/adapter/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+				//我啦2
         setContentView(R.layout.activity_main);
 		//我啦
     }
